95da7b1eb52b500d0e703fe243ea49251dc857dc
Étienne Vallette d'Osia <dohzya@gmail.com>
2016-08-26 11:33:09 +0200

    server: Fix a bit

--- server/app/actors/GameActor.scala
+++ server/app/actors/GameActor.scala
@@ -2,21 +2,30 @@ package actors
 
 import akka.actor._
 import play.api.libs.json._
+import play.api.Logger
 import scala.concurrent.{ ExecutionContext, Future, Promise }
 import scala.concurrent.ExecutionContext.Implicits.global
 
 object GameActor {
   def props() = Props(new GameActor())
+  val logger = Logger("application.player")
 }
 
 class GameActor() extends Actor {
+  import GameActor.logger
 
   val players = scala.collection.mutable.Set.empty[ActorRef]
 
   def gameInfo: JsValue = Json.obj("players" -> players.size)
 
-  def addPlayer(player: ActorRef) = players += player
-  def removePlayer(player: ActorRef) = players -= player
+  def addPlayer(player: ActorRef) = {
+    logger.info("Add Player")
+    players += player
+  }
+  def removePlayer(player: ActorRef) = {
+    logger.info("Remove Player")
+    players -= player
+  }
 
   def receive = {
     case GameInfo(p) => p.success(gameInfo)
--- server/app/actors/PlayerActor.scala
+++ server/app/actors/PlayerActor.scala
@@ -2,12 +2,18 @@ package actors
 
 import akka.actor._
 import play.api.libs.json._
+import play.api.Logger
+
+import models._
 
 object PlayerActor {
   def props(ws: ActorRef, game: ActorRef) = Props(new PlayerActor(ws, game))
+  val logger = Logger("application.player")
 }
 
 class PlayerActor(ws: ActorRef, game: ActorRef) extends Actor {
+  import PlayerActor.logger
+
   override def preStart() {
     game ! Connect(self)
   }
@@ -15,10 +21,12 @@ class PlayerActor(ws: ActorRef, game: ActorRef) extends Actor {
     game ! Disconnect(self)
   }
 
-
-
   def receive = {
-    case msg: JsValue =>
-      ws ! ("I received your message: " + msg)
+    case InEvent.Ping(msg) =>
+      logger.info(s"Received Ping($msg)")
+      ws ! OutEvent.Pong("I received your message: " + msg)
+
+    case err =>
+      logger.warn(s"Received invalid message: $err")
   }
 }
